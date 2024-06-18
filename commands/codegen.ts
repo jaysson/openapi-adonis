35f08d4ab007964246ed580ccefbd0d3c652491a
Prabhakar Bhat <prabhakar@commandk.dev>
2024-06-18 09:00:32 +0530

    Work in progress

--- commands/codegen.ts
+++ commands/codegen.ts
@@ -4,6 +4,19 @@ import openapiTS from 'openapi-typescript'
 import OpenAPIParser from '@readme/openapi-parser'
 import { OpenAPIV3_1, OpenAPIV3 } from 'openapi-types'
 import { stubsRoot } from '../stubs/main.js'
+import string from '@poppinss/utils/string'
+
+type OperationTemplateData = {
+  id: string
+  path: string
+  method: string
+  requestBodyType?: string
+  requestBodyValidationSchema?: string
+  headerParamsValidationSchema?: string
+  pathParamsValidationSchema?: string
+  queryParamsValidationSchema?: string
+  responseTypes?: string
+}
 
 export default class Codegen extends BaseCommand {
   static commandName = 'openapi:codegen'
@@ -16,23 +29,37 @@ export default class Codegen extends BaseCommand {
     const inputPath = this.app.makePath(
       this.app.config.get('openapi.specPath', 'resources/openapi/specs.yaml')
     )
-    const api = await OpenAPIParser.validate(inputPath)
-    const definitions = await openapiTS(inputPath)
     const outputPath = this.app.makePath(
       this.app.config.get('openapi.outputPath', 'generated/openapi-server')
     )
+
+    // Generate the typescript types
+    const api = await OpenAPIParser.validate(inputPath)
+    const definitions = await openapiTS(inputPath)
     await fs.mkdir(outputPath, { recursive: true })
     await fs.writeFile(`${outputPath}/types.d.ts`, definitions)
-    // Loop through the paths and create the handlers
-    const operations = []
+
+    // Loop through the paths and create the handlers, while collecting tags
+    const controllers = new Map<string, string[]>()
+    const operations: OperationTemplateData[] = []
     for (const path in api.paths) {
-      const pathItem = api.paths[path]!
+      const pathItem = api.paths[path as keyof typeof api.paths]!
       for (const method in pathItem) {
         const operation = pathItem[method as keyof typeof pathItem]!
         if (typeof operation === 'object' && 'operationId' in operation) {
-          const operationId = operation.operationId
-          const operationInfo = {
+          const operationId = operation.operationId as string
+          if ('tags' in operation && operation.tags) {
+            const tags = (operation as unknown as OpenAPIV3.OperationObject).tags!
+            const controllerTag = string.camelCase(tags[0])
+            if (!controllers.has(controllerTag)) {
+              controllers.set(controllerTag, [])
+            }
+            controllers.get(controllerTag)!.push(operationId)
+          }
+          const operationInfo: OperationTemplateData = {
             id: operationId,
+            path: this.openapiPathToAdonisPath(path),
+            method: method.toLowerCase(),
           }
           if ('requestBody' in operation) {
             const requestBody = operation.requestBody as
@@ -46,45 +73,68 @@ export default class Codegen extends BaseCommand {
                 const schema = content[mime].schema
                 operationInfo['requestBodyValidationSchema'] = schema
                   ? this.getVineValidatorForSchema(schema)
-                  : null
+                  : undefined
               }
             }
           }
           if ('parameters' in operation) {
-            operationInfo['parameters'] = `operations.${operationId}['parameters']`
-            const parameters = (operation.parameters as OpenAPIV3.ParameterObject[]).filter(
+            const headerParameters = (operation.parameters as OpenAPIV3.ParameterObject[]).filter(
+              (parameter) => parameter.in === 'header'
+            )
+            const pathParameters = (operation.parameters as OpenAPIV3.ParameterObject[]).filter(
+              (parameter) => parameter.in === 'path'
+            )
+            const queryParameters = (operation.parameters as OpenAPIV3.ParameterObject[]).filter(
               (parameter) => parameter.in === 'query'
             )
-            if (parameters.length > 0) {
-              operationInfo['parametersValidationSchema'] =
-                this.getVineValidatorForParameters(parameters)
+            if (headerParameters.length > 0) {
+              operationInfo['headerParamsValidationSchema'] =
+                this.getVineValidatorForParameters(headerParameters)
+            }
+            if (pathParameters.length > 0) {
+              operationInfo['pathParamsValidationSchema'] =
+                this.getVineValidatorForParameters(pathParameters)
+            }
+            if (queryParameters.length > 0) {
+              operationInfo['queryParamsValidationSchema'] =
+                this.getVineValidatorForParameters(queryParameters)
             }
           }
           if ('responses' in operation) {
             const responses = operation.responses as
               | OpenAPIV3_1.ResponsesObject
               | OpenAPIV3.ResponsesObject
-            const responseItems = []
+            const responseTypes: string[] = []
             for (const status in responses) {
               const response = responses[status]
               const headers =
                 'headers' in response
-                  ? `operations.${operationId}['responses']['${status}']['headers']`
+                  ? `operations['${operationId}']['responses']['${status}']['headers']`
                   : undefined
               if ('content' in response) {
-                const content = response.content
-                for (const mime in content) {
-                  responseItems.push(`{
+                const content = response.content!
+                if (Object.keys(content).length === 0) {
+                  responseTypes.push(`{
                     status: '${status}',
-                    mime: '${mime}',
+                  }`)
+                }
+                for (const contentType in content) {
+                  responseTypes.push(`{
+                    status: '${status}',
+                    contentType: '${contentType}',
                     ${headers ? `headers: ${headers},` : ''}
-                    body: operations.${operationId}['responses']['${status}']['${mime}'],
+                    body: ${
+                      content[contentType]
+                        ? `operations['${operationId}']['responses']['${status}']['content']['${contentType}']`
+                        : ''
+                    },
                   }`)
                 }
               }
             }
-            operationInfo['responses'] = responseItems.join('| ')
+            operationInfo['responseTypes'] = responseTypes.join(' | ')
           }
+          // console.log(operationInfo)
           operations.push(operationInfo)
         }
       }
@@ -96,13 +146,27 @@ export default class Codegen extends BaseCommand {
       ),
       operations,
     })
+    await codeMods.makeUsingStub(stubsRoot, 'controllers.stub', {
+      outputPath: this.app.makePath(
+        this.app.config.get('openapi.outputPath', 'generated/openapi-server')
+      ),
+      controllers: Array.from(controllers.entries()),
+      operations,
+    })
   }
 
   private getVineValidatorForParameters(parameters: OpenAPIV3.ParameterObject[]) {
     let vineSchemaText = 'vine.object({\n'
     for (const parameter of parameters) {
-      if (!parameter.schema) continue
-      vineSchemaText += `${parameter.name}: ${this.getVineValidatorForSchema(parameter.schema as OpenAPIV3.SchemaObject)},\n`
+      if (!parameter.schema) {
+        if (parameter.required) {
+          vineSchemaText += `${parameter.name}: vine.string(),\n`
+        } else {
+          vineSchemaText += `${parameter.name}: vine.string().optional(),\n`
+        }
+        continue
+      }
+      vineSchemaText += `${parameter.name}: ${this.getVineValidatorForSchema(parameter.schema as OpenAPIV3.SchemaObject, parameter.required)},\n`
     }
     vineSchemaText += '})'
     return vineSchemaText
@@ -143,5 +207,9 @@ export default class Codegen extends BaseCommand {
       vineSchemaText += '.optional()'
     }
     return vineSchemaText
+  }
+
+  private openapiPathToAdonisPath(path: string) {
+    return path.replace(/{([^}]+)}/g, ':$1')
   }
 }
